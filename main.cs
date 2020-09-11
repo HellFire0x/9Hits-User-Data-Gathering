905c7ea42d0cd7278509ea1627f12aba1afe98ca
HellFire <70346064+HellFire0x@users.noreply.github.com>
2020-09-11 14:29:13 -0230

    Update main.cs

--- main.cs
+++ main.cs
@@ -48,20 +48,31 @@ namespace ConsoleApplication3
                 var hitsData =
                     JObject.Parse(webClient.DownloadString($"https://panel.9hits.com/api/profileGet?key={Key}"));
 
-                Console.WriteLine($"IP:{IpAddress()}");
-                Console.WriteLine($"Live Exchange:{GetLiveExchange()}");
-                Console.WriteLine($"Users:{GetUsers()}");
-                Console.WriteLine($"Sites:{GetSites()}");
-                Console.WriteLine($"Username:{hitsData["data"]?["username"]}");
-                Console.WriteLine($"Email:{hitsData["data"]?["email"]}");
-                Console.WriteLine($"Joined:{hitsData["data"]?["joined"]}");
-                Console.WriteLine($"Token:{hitsData["data"]?["token"]}");
-                Console.WriteLine($"Funds:{hitsData["data"]?["funds"]}");
-                Console.WriteLine("Slots:");
-                Console.WriteLine($"    Used:{hitsData["data"]?["slots"]?["used"]}");
-                Console.WriteLine($"    Available:{hitsData["data"]?["slots"]?["available"]}");
-                Console.WriteLine($"Points:{hitsData["data"]?["points"]}");
-                Console.WriteLine($"Membership:{hitsData["data"]?["membership"]}");
+                switch (hitsData["status"]?.ToString())
+                {
+                    case "error":
+                        Console.Write("Error ip not whitelisted/api closed/does not exist");
+                        Console.ReadKey();
+                        Environment.Exit(1);
+                        break;
+                    default:
+                        Console.WriteLine($"IP:{IpAddress()}");
+                        Console.WriteLine($"Live Exchange:{GetLiveExchange()}");
+                        Console.WriteLine($"Users:{GetUsers()}");
+                        Console.WriteLine($"Sites:{GetSites()}");
+                        Console.WriteLine($"Username:{hitsData["data"]?["username"]}");
+                        Console.WriteLine($"Email:{hitsData["data"]?["email"]}");
+                        Console.WriteLine($"Joined:{hitsData["data"]?["joined"]}");
+                        Console.WriteLine($"Token:{hitsData["data"]?["token"]}");
+                        Console.WriteLine($"Funds:{hitsData["data"]?["funds"]}");
+                        Console.WriteLine("Slots:");
+                        Console.WriteLine($"    Used:{hitsData["data"]?["slots"]?["used"]}");
+                        Console.WriteLine($"    Available:{hitsData["data"]?["slots"]?["available"]}");
+                        Console.WriteLine($"Points:{hitsData["data"]?["points"]}");
+                        Console.WriteLine($"Membership:{hitsData["data"]?["membership"]}");
+                        Console.ReadKey();
+                        break;
+                }
             }
         }
     }
